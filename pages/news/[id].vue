5c9253384952575b1988de4d556bde0c6939c9b0
hejunv587 <hejunv587@163.com>
2023-11-20 00:13:17 +0800

    新闻页md

---
+++ pages/news/[id].vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="sm:pl-16 pl-8 top-0">
+        <div class="sm:px-16 px-8 sm:py-24 py-12 mx-auto">
+            <ContentDoc>
+                <template #not-found>
+                    <div>
+                        <h1>文档未找到</h1>
+                        <p>抱歉，请求的文档未找到。</p>
+                    </div>
+                </template>
+            </ContentDoc>
+        </div>
+    </section>
+</template>
+  
+<script setup>
+
+</script>
+
+<style scoped>
+img { 
+    display: inline-block;
+    margin: 0 auto !important;
+}
+</style>
